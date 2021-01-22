ef17408ebcc1d4d9df352be8e4efb7d0c7e65133
jinwook.im <jin3670@gmail.com>
2021-01-22 13:36:31 +0900

    Exception Handler

---
+++ test/src/main/java/com/gintire/test/exception/UserException.java
@@ -0,0 +1,13 @@
+package com.gintire.test.exception;/**
+ * Project: spring-boot-test
+ * Package: com.gintire.test.exception
+ * <p>
+ * @author: jin36
+ * @version: 
+ * Date: 2021-01-22
+ * Time: 오전 11:24
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */public class UserException {
+}
